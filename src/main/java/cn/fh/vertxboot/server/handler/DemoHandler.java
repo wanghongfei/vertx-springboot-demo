03754d8ab071e58dc6b831559748648dc6723af4
wanghongfei <brucewhf@gmail.com>
2018-11-11 14:35:25 +0800

    code demo

--- src/main/java/cn/fh/vertxboot/server/handler/DemoHandler.java
+++ src/main/java/cn/fh/vertxboot/server/handler/DemoHandler.java
@@ -1,21 +1,59 @@
 package cn.fh.vertxboot.server.handler;
 
 import cn.fh.vertxboot.service.DemoService;
+import io.vertx.core.CompositeFuture;
+import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.ext.web.RoutingContext;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
+@Slf4j
 public class DemoHandler implements Handler<RoutingContext> {
     @Autowired
     private DemoService demoService;
 
     @Override
     public void handle(RoutingContext route) {
-        route.response()
-                .end(
-                        demoService.hello(route.request().host())
+        log.info(route.request().path());
+
+        Future<String> fut1 = Future.future();
+        Future<String> fut2 = Future.future();
+
+        // 执行block调用
+        route.vertx()
+                .executeBlocking(
+                        fut -> {
+                            String result = demoService.blockingLogic(1);
+                            fut.complete(result);
+                        },
+                        fut1.completer()
                 );
+
+        // 执行block调用
+        route.vertx()
+                .executeBlocking(
+                        fut -> {
+                            String result = demoService.blockingLogic(2);
+                            fut.complete(result);
+                        },
+                        fut2.completer()
+                );
+
+        // 组合结果
+        CompositeFuture.all(fut1, fut2).setHandler(ar -> {
+            if (!ar.succeeded()) {
+                log.error("", ar.cause());
+                route.response().end("error");
+                return;
+            }
+
+            List<String> resultList = ar.result().list();
+            route.response().end(resultList.toString());
+        });
     }
 }
--- src/main/java/cn/fh/vertxboot/service/DemoService.java
+++ src/main/java/cn/fh/vertxboot/service/DemoService.java
@@ -1,10 +1,18 @@
 package cn.fh.vertxboot.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class DemoService {
     public String hello(String name) {
         return "hello, " + name;
+    }
+
+    public String blockingLogic(int i) {
+        log.info("execute blocking logic {}", i);
+
+        return i + " done";
     }
 }
