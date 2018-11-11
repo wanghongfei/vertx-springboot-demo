d22e9757e455f4f3dae7be9e7475fadf4f2c971e
wanghongfei <brucewhf@gmail.com>
2018-11-11 13:57:47 +0800

    comment

--- src/main/java/cn/fh/vertxboot/bootstrap/BootEventListener.java
+++ src/main/java/cn/fh/vertxboot/bootstrap/BootEventListener.java
@@ -15,6 +15,9 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
+/**
+ * 通过此监听器将vertx启动逻辑嵌入到Spring生命周期中
+ */
 @Component
 @Slf4j
 public class BootEventListener implements ApplicationListener<ApplicationStartedEvent> {
@@ -31,6 +34,9 @@ public class BootEventListener implements ApplicationListener<ApplicationStarted
         deploy(context, props.getServer());
     }
 
+    /**
+     * 此方法只是打印一下路由信息
+     */
     private void logMappings(ApplicationContext ctx, List<VertxProps.HandlerMapping> mappings) {
         for (VertxProps.HandlerMapping hm : mappings) {
             log.info("mapping {} {} to {}", hm.getMethod(), hm.getPath(),
@@ -40,7 +46,14 @@ public class BootEventListener implements ApplicationListener<ApplicationStarted
 
     }
 
+    /**
+     * 部署http verticle
+     *
+     * @param context
+     * @param config
+     */
     private void deploy(ApplicationContext context, VertxProps.Server config) {
+        // 创建vertx
         VertxOptions vertxOptions = new VertxOptions();
         vertxOptions.setEventLoopPoolSize(config.getNioThreadCount())
                 .setWorkerPoolSize(config.getWorkerThreadCount());
@@ -50,12 +63,17 @@ public class BootEventListener implements ApplicationListener<ApplicationStarted
         DeploymentOptions depOptions = new DeploymentOptions();
         depOptions.setInstances(config.getVerticleCount());
 
+        // 给verticle中的spring上下文引用赋值
         VertxHttpServerVerticle.springContext = context;
 
+        // latch用于等待部署完成
         CountDownLatch latch = new CountDownLatch(1);
         vertx.deployVerticle(VertxHttpServerVerticle.class, depOptions, ar -> {
+            // 部署失败
             if (!ar.succeeded()) {
+                // 记日志
                 log.error("err:", ar.cause());
+                // 将异常设置到成员变量中
                 this.bootError = ar.cause();
                 latch.countDown();
 
@@ -69,8 +87,11 @@ public class BootEventListener implements ApplicationListener<ApplicationStarted
         try {
             latch.await();
 
+            // 如果bootError不为空, 说明部署失败
             if (null != bootError) {
+                // 关闭vertx
                 vertx.close();
+                // 抛出异常, 触发spring启动失败
                 throw bootError;
             }
 
--- src/main/java/cn/fh/vertxboot/server/VertxProps.java
+++ src/main/java/cn/fh/vertxboot/server/VertxProps.java
@@ -10,23 +10,50 @@ import java.util.List;
 @ConfigurationProperties(prefix = "vertx")
 @Data
 public class VertxProps {
+    /**
+     * vertx http server配置
+     */
     private Server server;
 
+    /**
+     * 请求路由配置
+     */
     private List<HandlerMapping> handlerMappings;
 
     @Data
     public static class HandlerMapping {
+        /**
+         * 请求路径
+         */
         private String path;
+        /**
+         * 请求方法, 如GET, POST, PUT
+         */
         private String method;
+        /**
+         * 对应Spring里的bean名
+         */
         private String beanName;
     }
 
     @Data
     public static class Server {
+        /**
+         * vertx http server监听端口
+         */
         private int port = 8080;
 
+        /**
+         * NIO线程数, 默认为CPU逻辑核心数量
+         */
         private int nioThreadCount = Runtime.getRuntime().availableProcessors();
+        /**
+         * worker线程池大小
+         */
         private int workerThreadCount = Runtime.getRuntime().availableProcessors();
+        /**
+         * 部署的http verticle数量
+         */
         private int verticleCount = Runtime.getRuntime().availableProcessors();
     }
 }
